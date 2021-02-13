b8c9f1144f635ff2c7debf0d34faa57c63f05e4b
Mindaugas Dirma <mindaugas.dirma@gmail.com>
2021-02-13 19:52:56 +0200

    push

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Weather from "./components/Weather";
+import { API_KEY } from "./utils/WeatherApiKey";
+import { weatherConditions } from "./utils/weatherConditions";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [temperature, setTemperature] = useState(0);
+  const [weatherCondition, setWeatherCondition] = useState("Sunny");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+
+  fetchWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setTemperature(Math.round(json.main.temp));
+        setWeatherCondition(json.weather[0].main);
+        setLoading(false);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: weatherConditions[weatherCondition].color },
+      ]}
+    >
+      {isLoading ? (
+        <Text>Fetching The Weather</Text>
+      ) : (
+        <Weather weather={weatherCondition} temperature={temperature} />
+      )}
     </View>
   );
 }
@@ -14,8 +51,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ utils/WeatherApiKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = "8fa6ffa106b373c4bef232d06bb33ee6";
